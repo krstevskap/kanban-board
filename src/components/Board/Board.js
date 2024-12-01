00893cd7a1b3f97ce94e87dafab99c3eb7fa4dc5
krstevskap <krstevskap13@gmail.com>
2024-12-01 02:30:15 +0100

    Fix responsive design issues

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,31 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import Tasks from '../Tasks/Tasks';
-import ProgressBar from '../ProgressBar/ProgressBar';
-import './board.css';
+import React, { useState, useEffect } from "react";
+import Tasks from "../Tasks/Tasks";
+import ProgressBar from "../ProgressBar/ProgressBar";
+import "./board.css";
 
 const Board = () => {
+  const loadTasks = () => {
+    const savedTasks = localStorage.getItem("tasks");
+    return savedTasks ? JSON.parse(savedTasks) : [];
+  };
 
-    const loadTasks = () => {
-        const savedTasks = localStorage.getItem('tasks');
-        return savedTasks ? JSON.parse(savedTasks) : [];
-    };
+  const [tasks, setTasks] = useState(loadTasks);
 
-    const [tasks, setTasks] = useState(loadTasks);
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
-    useEffect(() => {
-        localStorage.setItem('tasks', JSON.stringify(tasks));
-    }, [tasks]);
+  return (
+    <div className="board-container">
+      <div className="left-column">
+        <Tasks tasks={tasks} setTasks={setTasks} />
+      </div>
+      <div className="right-column">
+        <ProgressBar tasks={tasks} />
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <div className='board-container'>
-            <div className='left-column'>
-                <Tasks tasks={tasks} setTasks={setTasks} />
-            </div>
-            <div className='right-column'>
-                <ProgressBar tasks={tasks} />
-            </div>
-        </div>
-    )
-}
-
-export default Board
+export default Board;
